bdc66454f01f5a42c32f2aadc6df4f195937c594
Macauley Drake <macauleyann@gmail.com>
2018-09-24 18:26:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  if(distance<400){
+  }
+}
+function ternaryCheckCity(city) {
+  
+}
+function switchOnCharmFromTip(tip){
+  
+}
