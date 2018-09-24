678e143fc716cea49b9f2619fc02707cd75e1563
Macauley Drake <macauleyann@gmail.com>
2018-09-24 18:44:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function scuberGreetingForFeet(distance) {
   if(distance<=400){
     return"This one is on me!"
   }
-  else if (){
-    
+  else if(distance){
+  
   }
 }
 function ternaryCheckCity(city) {
