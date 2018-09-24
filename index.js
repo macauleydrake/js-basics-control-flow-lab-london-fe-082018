01945cfa6a282baf4a1fdb052b9a82a6d4caa660
Macauley Drake <macauleyann@gmail.com>
2018-09-24 19:12:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function ternaryCheckCity(city) {
 function switchOnCharmFromTip(tip){
   switch (tip) {
     case "generous":
-      return ("Thank you so much")
+      return ("Thank you so much.")
       break;
     case "not as generous":
       return ("Thank you.")
