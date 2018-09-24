9c26d7a338fe6558e4cb93cd9b2a425d8cb54ae4
Macauley Drake <macauleyann@gmail.com>
2018-09-24 18:56:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function scuberGreetingForFeet(distance) {
 }
 
 function ternaryCheckCity(city) {
-  city == "NYC" ?  "Ok, sounds good." : "No go."
+  return city == "NYC" ?  "Ok, sounds good." : "No go."
   
 }
 function switchOnCharmFromTip(tip){
